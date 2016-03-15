a12e74e3400e300e2379dbaae581b3eca8ce06f7
Henry Chan <henry.chan@tigerspike.com>
2016-03-15 14:12:36 -0400

    Initial commit

---
+++ real-time-location-socket-io/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  real-time-location-socket-io
+//
+//  Created by Henry Chan on 3/15/16.
+//  Copyright © 2016 Henry Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ server/index.js
@@ -0,0 +1,24 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(3000, function(){
+  console.log('Listening on *:3000');
+});
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function (socket) {
+    console.log(data);
+});
